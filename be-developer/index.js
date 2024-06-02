3fb5eb0562b1dcece4bf7adce64496c13d864cc1
ziagyfernando1 <ziagyfernando1@students.amikom.ac.id>
2024-06-02 21:25:10 +0700

    ubah index

--- be-developer/index.js
+++ be-developer/index.js
@@ -5,7 +5,7 @@ const express = require("express");
 
 const usersRoutes = require("./routes/users.js");
 
-const MiddlewareLogRequest = require("./middleware/logs");
+const MiddlewareLogRequest = require("./middlewares/logs");
 
 const app = express();
 
